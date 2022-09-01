f0fcad7ac5522634af9a365116cf7cd29105574a
Vitória <vitcs11@gmail.com>
2022-08-31 23:44:19 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const button = document.getElementById('button-random-color');
-const corAleatoria = document.querySelectorAll('#corAleatoria');
 
 // Função de gerar cor aleatória retirada do site hora de codar.
 function generateColor() {
@@ -13,10 +12,20 @@ function generateColor() {
 }
 
 function defineCoresAleatorias() {
-  for (let index = 0; index < corAleatoria.length; index += 1) {
-    corAleatoria[index].style.backgroundColor = generateColor();
+  const colors = document.querySelectorAll('.color');
+  for (let index = 1; index < colors.length; index += 1) {
+    colors[index].style.backgroundColor = generateColor();
   }
-  console.log('teste');
+  const newColors = document.getElementById('color-palette');
+  localStorage.setItem('colorPalette', newColors.innerHTML);
 }
 
 button.addEventListener('click', defineCoresAleatorias);
+
+window.onload = function () {
+  const colorPalette = document.getElementById('color-palette');
+  const colorPaletteLocalStorage = localStorage.getItem('colorPalette');
+  if (colorPaletteLocalStorage !== undefined) {
+    colorPalette.innerHTML = colorPaletteLocalStorage;
+  }
+};
